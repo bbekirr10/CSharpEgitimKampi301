67e36737a29ead66b2cd4e390be7c47509864037
bbekirr10 <bbekirr_yavuz@hotmail.com>
2024-11-24 22:10:06 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EFProject/FrmLocation.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi301.EFProject
+{
+    public partial class FrmLocation : Form
+    {
+        public FrmLocation()
+        {
+            InitializeComponent();
+        }
+        EgitimKampiEfTravelDbEntities db = new EgitimKampiEfTravelDbEntities();
+
+
+        private void FrmLocation_Load(object sender, EventArgs e)
+        {
+            var values = db.tblGuide.Select(x => new
+            {
+                FullName = x.GuideName + " " + x.GuideSurname,
+                x.GuideId
+            }).ToList() ;
+            cmdGuide.DisplayMember = "FullName";
+            cmdGuide.ValueMember = "GuideId";
+            cmdGuide.DataSource = values;
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var values = db.tblLocation.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            tblLocation location = new tblLocation();
+            location.Capacity = byte.Parse(nudCapacity.Value.ToString());
+            location.City = txtCity.Text;
+            location.Country = txtCountry.Text;
+            location.Price = Decimal.Parse(txtPrice.Text);
+            location.DayNight = txtDayNight.Text;
+            location.GuideId = int.Parse(cmdGuide.SelectedValue.ToString());
+            db.tblLocation.Add(location);
+            db.SaveChanges();
+            MessageBox.Show("Ekleme işlemi başarılı...");
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var deletedValue = db.tblLocation.Find(id);
+            db.tblLocation.Remove(deletedValue);
+            db.SaveChanges();
+            MessageBox.Show("Silme işlemi başarılı...");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var updatedValue = db.tblLocation.Find(id);
+            updatedValue.DayNight = txtDayNight.Text;
+            updatedValue.Price = int.Parse(txtPrice.Text);
+            updatedValue.Capacity = byte.Parse(nudCapacity.Value.ToString());
+            updatedValue.City = txtCity.Text;
+            updatedValue.Country = txtCountry.Text;
+            updatedValue.GuideId = int.Parse(cmdGuide.SelectedValue.ToString());
+            db.SaveChanges();
+            MessageBox.Show("Güncelleme işlemi başarılı...");
+        }
+    }
+}
---
+++ CSharpEgitimKampi301.EFProject/FrmStatistics.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi301.EFProject
+{
+    public partial class FrmStatistics : Form
+    {
+        public FrmStatistics()
+        {
+            InitializeComponent();
+        }
+        EgitimKampiEfTravelDbEntities db = new EgitimKampiEfTravelDbEntities();
+        private void FrmStatistics_Load(object sender, EventArgs e)
+        {
+            lblLocationCount.Text = db.tblLocation.Count().ToString();
+            lblTotalCapacity.Text = db.tblLocation.Sum(x=>x.Capacity).ToString();
+            lblGuideCount.Text = db.tblGuide.Count().ToString();
+            lblAvgCapacity.Text = db.tblLocation.Average(x=>x.Capacity).ToString();
+            decimal averageLocationPrice =decimal.Parse(db.tblLocation.Average(x=>x.Price).ToString());
+            lblAvgLocationPrice.Text = Math.Round(averageLocationPrice, 2).ToString();
+
+            int lastCountryId = db.tblLocation.Max(x => x.LocationId);
+            lblLastCountry.Text = db.tblLocation.Where(x => x.LocationId == lastCountryId).Select(y=>y.Country).FirstOrDefault();
+
+            lblCappadocia.Text = db.tblLocation.Where(x => x.City=="Kapadokya").Select(y=>y.Capacity).FirstOrDefault().ToString();
+            lblTurkiyeLocationCapacityAverage.Text = db.tblLocation.Where(x => x.Country == "Türkiye").Average(y => y.Capacity).ToString();
+
+            var guideRomeId = db.tblLocation.Where(x => x.City == "Roma Turistlik").Select(y => y.GuideId).FirstOrDefault();
+            lblRomeGuide.Text = db.tblGuide.Where(x=>x.GuideId==guideRomeId).Select(y=>y.GuideName+" "+y.GuideSurname).FirstOrDefault();
+
+            var maxCapacityLocation = db.tblLocation.Max(x => x.Capacity);
+            lblMaxCapacityLocation.Text = db.tblLocation.Where(x => x.Capacity == maxCapacityLocation).Select(y => y.City).FirstOrDefault();
+
+            var maxPriceLocation = db.tblLocation.Max(x => x.Price);
+            lblMaxPriceLocation.Text = db.tblLocation.Where(x => x.Price == maxPriceLocation).Select(y => y.City).FirstOrDefault();
+
+            var guideBeyzaKara = db.tblGuide.Where(x => x.GuideName == "Beyza" && x.GuideSurname == "Kara").Select(y => y.GuideId).FirstOrDefault();
+            lblBeyzaKara.Text = db.tblLocation.Where(x => x.GuideId == guideBeyzaKara).Count().ToString();
+
+        }
+    }
+}
